9c47bd3d9dddc71e64069a430fde62dc642e1675
AayushGajbhiye9860 <aayushgajbhiye9860@gmail.com>
2026-05-01 20:33:14 +0530

    Add MoveTable component

---
+++ frontend/src/components/MoveTable.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MoveTable() {
+  return (
+    <div className="p-4 border border-black bg-gray-50">
+      <p className="font-mono text-sm text-gray-500 text-center">Move Table Component</p>
+    </div>
+  );
+}
