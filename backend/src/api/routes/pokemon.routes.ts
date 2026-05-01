85988c1acc096c0ccf476beab46cdf0775969eec
AayushGajbhiye9860 <aayushgajbhiye9860@gmail.com>
2026-05-01 14:24:01 +0530

    feat: commit backend/src/api/routes/pokemon.routes.ts

--- backend/src/api/routes/pokemon.routes.ts
+++ backend/src/api/routes/pokemon.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import * as pokemonController from '../controllers/pokemon.controller';
+import { cacheMiddleware } from '../middleware/cache.middleware';
 
 const router = Router();
 
-router.get('/', pokemonController.getAllPokemon);
-router.get('/:slug', pokemonController.getPokemonBySlug);
+router.get('/', cacheMiddleware(3600), pokemonController.getAllPokemon);
+router.get('/:slug', cacheMiddleware(86400), pokemonController.getPokemonBySlug);
+router.get('/:id/navigation', cacheMiddleware(86400), pokemonController.getNextPrevPokemon);
 
 export default router;
